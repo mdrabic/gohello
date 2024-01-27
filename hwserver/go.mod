b56daf4a1c3980d9040508644eea53f508aa07a5
Mike Drabic <mdrabic@gmail.com>
2024-01-27 15:33:14 -0500

    GET /hello

---
+++ hwserver/go.mod
@@ -0,0 +1,3 @@
+module github.com/mdrabic/gohello/hwserver 
+
+go 1.21.5
--- hwserver/hwserver.go
+++ hwserver/hwserver.go
@@ -1,10 +1,17 @@
-package hwserver 
+package main
 
 import (
 	"log"
 	"net/http"
+	"io"
 )
 
 func main() {
+	getHello := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, "Hello!")
+	}
+
+	http.HandleFunc("/hello", getHello)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }

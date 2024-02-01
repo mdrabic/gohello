59bd093f631f5dca75e676521c653b2973b36e57
Mike Drabic <mdrabic@gmail.com>
2024-01-31 20:46:11 -0500

    comments

--- hwserver/wordcount_test.go
+++ hwserver/wordcount_test.go
@@ -11,6 +11,7 @@ func TestWordCount(t *testing.T) {
 	expected := map[string]int{"Go!": 1, "I": 1, "am": 1, "learning": 1}
 
 	//fmt.Sprint will print the maps keys in key-sorted order
+	//could checkout https://github.com/google/go-cmp as a better first class solution
 	if fmt.Sprint(output) != fmt.Sprint(expected) {
 		t.Errorf("Epected to find needle but got false")
 	}
